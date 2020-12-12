a4b1b4dd6cd4d24650132cf5701e5071b1f664d6
guanweiming <guanweiming@yunji.ai>
2020-12-12 11:32:39 +0800

    common

--- common/src/main/java/com/coding/service/PowerService.java
+++ common/src/main/java/com/coding/service/PowerService.java
@@ -2,11 +2,14 @@ package com.coding.service;
 
 
 import com.github.lianjiatech.retrofit.spring.boot.annotation.RetrofitClient;
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Commit;
 import retrofit2.http.GET;
 
 /**
  * @author yunji
  */
+@Component
 @RetrofitClient(baseUrl = "https://power.coding-space.cn/")
 public interface PowerService {
 
