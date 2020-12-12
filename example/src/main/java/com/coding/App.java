70681fd1914f0635b58e0bf6e505714f16c63170
guanweiming <guanweiming@yunji.ai>
2020-12-12 11:16:37 +0800

    power

--- example/src/main/java/com/coding/App.java
+++ example/src/main/java/com/coding/App.java
@@ -1,27 +1,40 @@
 package com.coding;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.coding.service.PowerService;
+import com.github.lianjiatech.retrofit.spring.boot.annotation.RetrofitScan;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@RetrofitScan("com.coding")
 @EnableSwagger2
 @RestController
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
 
     @ApiModelProperty
     private String demo;
     @Autowired
     private AmazonS3 s3;
+    @Autowired
+    private PowerService powerService;
 
     public static void main(String[] args) {
         Jackson2ObjectMapperBuilder s;
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("start");
+        Object list = powerService.getList();
+        System.out.println(list);
     }
 }
